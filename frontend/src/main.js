406ae3c5deb8482081b4ec597534514123ff7781
Pedro Motta <76851399+ipedromotta@users.noreply.github.com>
2022-10-14 16:42:57 -0300

    :wrench: Axios config base url

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,9 +3,14 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import axios from 'axios'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.min.js'
+
+const host = window.location.hostname
+const port = window.location.port
+axios.defaults.baseURL = `http://${host}:${port}`
 
 const app = createApp(App)
 
