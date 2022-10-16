51f51ba12d02134317f6a1527a946fd772f4180c
Pedro Motta <76851399+ipedromotta@users.noreply.github.com>
2022-10-16 19:39:35 -0300

    :wrench: chore: Add JWT links

--- backend/configs/urls.py
+++ backend/configs/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('djoser.urls')),
+    path('api/v1/', include('djoser.urls.jwt')),
     path('api/v1/', include('djoser.urls.authtoken')),
 ]
