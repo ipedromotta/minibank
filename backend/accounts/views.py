1f3605e3c27dfec50e7dea9f7e32e849e6bd00cd
Pedro Motta <76851399+ipedromotta@users.noreply.github.com>
2022-10-21 18:59:02 -0300

    :recycle: refactor: Melhorias no codigo

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -9,49 +9,45 @@ from .models import CustomUser
 @api_view(['POST'])
 def deposit(request):
     try:
+        user = None
         username = request.user
         amount = request.data.get('amount', None)
         if amount and username:
-            user = CustomUser.objects.filter(username=username)
-            for attributes in user:
-                attributes.balance += Decimal(amount)
-                attributes.save()
-                message = 'Deposito realizado com sucesso'
-                status = 200
+            user = CustomUser.objects.get(username=username)
+            user.balance += Decimal(amount)
+            user.save()
+            message = 'Deposito realizado com sucesso'
+            status = 200
         else:
             message = 'Algo deu errado'
             status = 500
-            attributes = None
     except:
         message = 'Algo deu errado'
         status = 500
-        attributes = None
     
-    return Response({"status": status, "response": message, "accountBalance": attributes.balance if attributes else None})
+    return Response({"status": status, "response": message, "accountBalance": user.balance if user else None})
 
 @api_view(['POST'])
 def withdraw(request):
     try:
+        user = None
         username = request.user
         amount = request.data.get('amount', None)
         if amount and username:
-            user = CustomUser.objects.filter(username=username)
-            for attributes in user:
-                if attributes.balance < Decimal(amount):
-                    message = 'Saldo insuficiente'
-                    status = 401
-                else:
-                    attributes.balance -= Decimal(amount)
-                    attributes.save()
-                    message = 'Saque realizado com sucesso'
-                    status = 200
+            user = CustomUser.objects.get(username=username)
+            if user.balance < Decimal(amount):
+                message = 'Saldo insuficiente'
+                status = 401
+            else:
+                user.balance -= Decimal(amount)
+                user.save()
+                message = 'Saque realizado com sucesso'
+                status = 200
         else:
             message = 'Algo deu errado'
             status = 500
-            attributes = None
     except:
         message = 'Algo deu errado'
         status = 500
-        attributes = None
                 
-    return Response({"status": status, "response": message, "accountBalance": attributes.balance if attributes else None})
+    return Response({"status": status, "response": message, "accountBalance": user.balance if user else None})
