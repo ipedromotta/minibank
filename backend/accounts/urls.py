6ba475750050e6b9870d751a756cfe08e9baf90c
Pedro Motta <76851399+ipedromotta@users.noreply.github.com>
2022-10-22 11:18:23 -0300

    :sparkles: feat: Criacao endpoint para transferencia entre contas

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('deposit/', views.deposit),
-    path('withdraw/', views.withdraw)
+    path('withdraw/', views.withdraw),
+    path('transfer/', views.transfer),
 ]
--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,5 +1,7 @@
 from decimal import Decimal
 
+from django.db import transaction
+
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, authentication_classes, permission_classes
 
@@ -14,7 +16,7 @@ def deposit(request):
         amount = request.data.get('amount', None)
         if amount and username:
             user = CustomUser.objects.get(username=username)
-            user.balance += Decimal(amount)
+            user.balance += abs(Decimal(amount))
             user.save()
             message = 'Deposito realizado com sucesso'
             status = 200
@@ -39,7 +41,7 @@ def withdraw(request):
                 message = 'Saldo insuficiente'
                 status = 401
             else:
-                user.balance -= Decimal(amount)
+                user.balance -= abs(Decimal(amount))
                 user.save()
                 message = 'Saque realizado com sucesso'
                 status = 200
@@ -51,3 +53,41 @@ def withdraw(request):
         status = 500
                 
     return Response({"status": status, "response": message, "accountBalance": user.balance if user else None})
+
+@api_view(['POST'])
+# @authentication_classes([])
+# @permission_classes([])
+def transfer(request):
+    try:
+        user = None
+        user_to = None
+        username = request.user
+        username_to = request.data.get('username_to', None)
+        amount = request.data.get('amount', None)
+        
+        if username and username_to and amount:
+            user = CustomUser.objects.get(username=username)
+            user_to = CustomUser.objects.get(username=username_to)
+
+            if user.balance < Decimal(amount):
+                message = 'Saldo insuficiente'
+                status = 401
+            else:
+                with transaction.atomic():
+                    user.balance -= abs(Decimal(amount))
+                    user.save()
+                    
+                    user_to.balance += abs(Decimal(amount))
+                    user_to.save()
+                    
+                    message = 'Transferencia realizada com sucesso'
+                    status = 200
+        else:
+            message = 'Algo deu errado'
+            status = 500
+    except:
+        message = 'Algo deu errado' if user_to is not None else 'Usuario destino não existe'
+        status = 500
+    
+    return Response({"status": status, "response": message, "accountBalance": user.balance if user else None})
+    
