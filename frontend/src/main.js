ad2c865bcaab3ec21b0e2c1d2c92a3d1a6846b97
Pedro Motta <76851399+ipedromotta@users.noreply.github.com>
2025-07-31 21:51:02 -0300

    🔄 chore: adaptacao do main.js para rodar com Docker

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -8,11 +8,9 @@ import axios from 'axios'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.min.js'
 
-// const host = window.location.hostname
-// const port = window.location.port
-// axios.defaults.baseURL = `http://${host}:${port}` // Produção
+// axios.defaults.baseURL = 'http://localhost:8000'
 
-axios.defaults.baseURL = 'http://localhost:8000'
+axios.defaults.baseURL = import.meta.env.VITE_API_URL // Produção
 
 const app = createApp(App)
 
